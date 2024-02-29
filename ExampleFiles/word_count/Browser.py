dec271ef84525263e4609dad8f079269039d78d7
Guido Demmenie <30650086+guidodemmenie@users.noreply.github.com>
2024-02-29 16:23:38 +0100

    See if this works

---
+++ ExampleFiles/word_count/Browser.py
@@ -0,0 +1,33 @@
+from robot.api.deco import keyword,library
+
+@library
+class browser:
+    @keyword
+    def new_browser(browser, headless):
+        return None
+    
+    @keyword
+    def new_context(viewport):
+        return None
+    
+    @keyword
+    def new_page(url):
+        return None
+    
+    @keyword
+    def close_page()
+        return None
+
+    @keyword
+    def close_context():
+        return None
+    
+    @keyword
+    def close_browser():
+        return None
+    
+    @keyword
+    def get_text(locator):
+        return somestringstuff
+    
+    
