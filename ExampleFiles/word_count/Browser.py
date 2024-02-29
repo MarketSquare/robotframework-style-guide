be26a25da78ad99b084d8d80fc0b21b9a141b24b
Guido Demmenie <30650086+guidodemmenie@users.noreply.github.com>
2024-02-29 16:51:20 +0100

    self

--- ExampleFiles/word_count/Browser.py
+++ ExampleFiles/word_count/Browser.py
@@ -3,30 +3,30 @@ from robot.api.deco import keyword,library
 @library
 class Browser:
     @keyword
-    def new_browser(browser, headless):
+    def new_browser(self, browser, headless):
         return None
     
     @keyword
-    def new_context(viewport):
+    def new_context(self, viewport):
         return None
     
     @keyword
-    def new_page(url):
+    def new_page(self, url):
         return None
     
     @keyword
-    def close_page():
+    def close_page(self):
         return None
 
     @keyword
-    def close_context():
+    def close_context(self):
         return None
     
     @keyword
-    def close_browser():
+    def close_browser(self):
         return None
     
     @keyword
-    def get_text(locator):
+    def get_text(self, locator):
         return None
     
